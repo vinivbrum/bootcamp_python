ea9cfb8c1ac51fcf13d40ddf8a93ef0a3cb6ed23
Vinivbrum <vinivbrum@gmail.com>
2024-05-04 16:47:45 -0300

    novos exercicios

--- main.py
+++ main.py
@@ -72,39 +72,105 @@
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
-# Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
-transacao = [{'valor': 12000, 'hora': 20},
-            {'valor': 800, 'hora': 12},
-            {'valor': 1000, 'hora': 22},
-            {'valor': 200, 'hora': 18},
-            {'valor': 12000, 'hora': 10},
-            {'valor': 1000, 'hora': 11},
-            {'valor': 100, 'hora': 21},
-            {'valor': 12000, 'hora': 8}]
+# # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+# transacao = [{'valor': 12000, 'hora': 20},
+#             {'valor': 800, 'hora': 12},
+#             {'valor': 1000, 'hora': 22},
+#             {'valor': 200, 'hora': 18},
+#             {'valor': 12000, 'hora': 10},
+#             {'valor': 1000, 'hora': 11},
+#             {'valor': 100, 'hora': 21},
+#             {'valor': 12000, 'hora': 8}]
 
-transacao_suspeita = []
+# transacao_suspeita = []
 
-for suspeitos in transacao:
-    if suspeitos['valor'] >= 10000 or suspeitos['hora'] < 9 or suspeitos['hora'] > 18:
-        transacao_suspeita.append(suspeitos)
-    else:
-        pass
-print (transacao_suspeita)
+# for suspeitos in transacao:
+#     if suspeitos['valor'] >= 10000 or suspeitos['hora'] < 9 or suspeitos['hora'] > 18:
+#         transacao_suspeita.append(suspeitos)
+#     else:
+#         pass
+# print (transacao_suspeita)
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
 
+
+# texto = str("Lorem Morbi dolor sit Morbi, consectetur adipiscing elit. In faucibus non libero ac tempus. In eu Morbi ipsum. Sed elementum dignissim felis vitae maximus. Morbi non urna nec neque sodales hendrerit. Sed iaculis tincidunt massa sed aliquam. Morbi nec justo nunc. Morbi pellentesque erat risus, a laoreet ex mollis vitae.Pellentesque auctor ante nisi, id condimentum risus maximus sed. Nullam elementum, diam non posuere auctor, magna enim vulputate nulla, sit amet iaculis quam dolor nec risus. Aenean sit amet neque ipsum. Quisque sed lectus ullamcorper, pulvinar nulla at, condimentum orci. Proin quam nibh, sodales sit amet magna porta, ultricies placerat eros. Praesent ultricies ullamcorper faucibus. Morbi sagittis a sem lobortis commodo. Donec tellus dui, tempor sed venenatis sed, cursus eget ante.")
+# result = {}
+# import string
+# def limpar_texto(text):
+#     text_replace = text.translate(str.maketrans('','',string.punctuation))
+#     print(text_replace)
+#     text_lower = text_replace.lower()
+#     text_split = text_lower.split()
+#     return text_split
+
+# palavras = limpar_texto(texto)
+
+
+# for palavra in palavras:
+#     if palavra in result:
+#         result[palavra] += +1
+#     else:
+#         result[palavra] = 1
+# print(result)
+        
+
+
+
 ### Exercício 7. Normalização de Dados
-# Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+# # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+# lista = [1,5,6,8,9,12,15,16]
+# min_lista = min(lista)
+# max_lista = max(lista)
+# norm = []
+
+# for i in lista:
+#     new_value = (i - min_lista)/(max_lista - min_lista)
+#     norm.append(new_value)
+# print(norm)
+
+
+
 
 ### Exercício 8. Filtragem de Dados Faltantes
 # Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
 
+
+
+
+
+
 ### Exercício 9. Extração de Subconjuntos de Dados
 # Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
+# lista = [1,5,6,8,9,12,15,16]
+# pares = []
+
+# for i in lista:
+#     if i%2 == 0:
+#         pares.append(i)
+# print(pares)
+
+
 
 ### Exercício 10. Agregação de Dados por Categoria
 # Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+vendas =  [{'categoria':"Carro",'valor':210},
+    {'categoria':"Casa",'valor':250},
+    {'categoria':"Carro",'valor':255},
+    {'categoria':"Casa",'valor':222},
+    {'categoria':"Carro",'valor':1200},
+    {'categoria':"Carro",'valor':203},
+    {'categoria':"Carro",'valor':205},
+    {'categoria':"Casa",'valor':226},
+    {'categoria':"Casa",'valor':212},
+    {'categoria':"Carro",'valor':200}]
+
+result = {}
+
+for venda in vendas:
+    if 
+    
 
 ### Exercícios com WHILE
 
